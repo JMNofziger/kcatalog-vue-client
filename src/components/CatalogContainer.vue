68cf4ddb2aac947d3b17266116315c9e4c5e7300
Jacob Nofziger <jacob.nofziger@gmail.com>
2021-09-09 16:05:05 -0600

    Fixes and updates syling across app, centers filter input

--- src/components/CatalogContainer.vue
+++ src/components/CatalogContainer.vue
@@ -1,29 +1,25 @@
 <template>
-  <div class="catalog-container">
-    <b-form-group
-        label="Filter"
-        label-for="filter-input"
-        label-cols-sm="3"
-        label-align-sm="right"
-        label-size="sm"
-        class="mb-0"
-      >
-      <b-input-group size="sm">
-        <b-form-input
-          id="filter-input"
-          v-model="filter"
-          type="search"
-          placeholder="Type to Search"
-        ></b-form-input>
+<div class="catalog-container">
+  <b-container fluid>
+  <b-row>
+    <b-col lg="6" offset-lg="3">
+    <b-form-group description="Filter the catalog with keywords">
+      <b-input-group size="md">
+        <b-form-input id="filter-input" v-model="filter" type="search" placeholder="Type to Search">
+        </b-form-input>
 
         <b-input-group-append>
           <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
         </b-input-group-append>
       </b-input-group>
     </b-form-group>
-    <b-table striped hover :filter="filter" :fields=fields :items="items">
-    </b-table>
-  </div>
+    </b-col>
+  </b-row>
+  </b-container>
+
+  <b-table striped hover :filter="filter" :fields=fields :items="items">
+  </b-table>
+</div>
 </template>
 
 <script>
